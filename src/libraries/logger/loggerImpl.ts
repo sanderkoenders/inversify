784a1db8c6efd47a7f7b796a01d6c6ded989f5a0
Sander Koenders <sanderkoenders@gmail.com>
2018-09-11 17:56:27 +0200

    refactor(LoggerImpl): make loggerImpl more testable

--- src/libraries/logger/loggerImpl.ts
+++ src/libraries/logger/loggerImpl.ts
@@ -1,18 +1,21 @@
 import TYPES from '../../constants/types';
 import Logger from './logger';
 import { inject, provideSingleton } from '../../ioc/iocUtils';
+import 'reflect-metadata';
 
 /* tslint:disable:no-console */
 @provideSingleton(TYPES.Logger)
 class LoggerImpl implements Logger {
   private appName: string;
+  private consoleRef: any;
 
-  public constructor(@inject(TYPES.Environment) environment: any) {
+  public constructor(@inject(TYPES.Environment) environment: any, @inject(TYPES.Console) consoleRef: any) {
+    this.consoleRef = consoleRef || console;
     this.appName = environment.appName;
   }
 
   public log(message: string) {
-    console.log(this.appName + ' says: ' + message);
+    this.consoleRef.log(this.appName + ' says: ' + message);
   }
 }
 
